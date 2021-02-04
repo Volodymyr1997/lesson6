718e2f8dbbbffe7055cb51056bb552fd18e359a9
Volodymyr1997 <vpochtaua@gmail.com>
2021-02-04 20:19:46 +0200

    lesson5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+////  import * as path from 'path';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user = {name: 'vova@gfh', age: 33 , password: 12345678} ;
+
   title = 'lesson6';
+
+////oninputAction() {
+ //// console.log(this.title)
+///}
+
+  //submit(form: NgForm ): void {
+  //console.log(form);
+
+  //}
+///}
+  age = this.user.age
+name = new FormControl( [this.user.name], [Validators.required, Validators.maxLength(8), this.custemFukcValidator, Validators.email]);
+passworld = new FormControl([this.user.password], [Validators.required, Validators.minLength(8)]);
+myForm = new FormGroup(
+  {name: this.name,
+  passworld: this.passworld}
+);
+xxx(myForm: FormGroup): void {
+  console.log(myForm)
+  }
+  custemFukcValidator(inputData: AbstractControl): any {
+
+  if (inputData.value === 'fukc'){
+    console.log(inputData);
+    return{error: true, msg: 'fukc is present'}; }
+  else {
+    return null;
+  }
+  }
+
+
 }
